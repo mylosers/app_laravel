fcb648a0ec2f8d7711c4e412b39eb3c8aef1fca7
losers <losers@localhost.localdomain>
2019-05-24 09:13:48 +0800

    完善主页

--- app/Http/Controllers/Home/HomeController.php
+++ app/Http/Controllers/Home/HomeController.php
@@ -16,18 +16,22 @@ class HomeController extends Controller
      * 首页视图
      */
     public function home(){
+        //查询商品表
         $goods=GoodsModel::where(['is_new'=>1,'is_up'=>1])->get();
         if($goods){
             $goods=$goods->toArray();
         }else{
             $goods=[];
         }
+        //查询商品热度
         $hos=GoodsModel::where(['is_hot'=>1,'is_up'=>1])->get();
         if($hos){
             $hos=$hos->toArray();
         }else{
             $hos=[];
         }
+        //购物车
+
         return view('Home.home',['data'=>$goods,'hos'=>$hos]);
     }
 }
