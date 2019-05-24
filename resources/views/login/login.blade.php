d3555c01a33fd9088a934a9ba36f747934cd916f
liuyong <1641628122@qq.com>
2019-05-24 14:41:17 +0800

    laylouts

--- resources/views/login/login.blade.php
+++ resources/views/login/login.blade.php
@@ -1,83 +1,12 @@
-<!DOCTYPE html>
-<html lang="zxx">
-<head>
-    <meta charset="UTF-8">
-    <title>Mstore - Online Shop Mobile Template</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1  maximum-scale=1 user-scalable=no">
-    <meta name="mobile-web-app-capable" content="yes">
-    <meta name="apple-mobile-web-app-capable" content="yes">
-    <meta name="apple-touch-fullscreen" content="yes">
-    <meta name="HandheldFriendly" content="True">
+@extends('layouts.app')
 
-    <link rel="stylesheet" href="/css/materialize.css">
-    <link rel="stylesheet" href="/font-awesome/css/font-awesome.min.css">
-    <link rel="stylesheet" href="/css/normalize.css">
-    <link rel="stylesheet" href="/css/owl.carousel.css">
-    <link rel="stylesheet" href="/css/owl.theme.css">
-    <link rel="stylesheet" href="/css/owl.transitions.css">
-    <link rel="stylesheet" href="/css/fakeLoader.css">
-    <link rel="stylesheet" href="/css/animate.css">
-    <link rel="stylesheet" href="/css/style.css">
+@section('title', '登录')
 
-    <link rel="shortcut icon" href="/img/favicon.png">
+@section('sidebar')
+    @parent
+@endsection
 
-</head>
-<body>
-
-<!-- navbar top -->
-<div class="navbar-top">
-    <!-- site brand	 -->
-    <div class="site-brand">
-        <a href="index.html"><h1>Mstore</h1></a>
-    </div>
-    <!-- end site brand	 -->
-    <div class="side-nav-panel-right">
-        <a href="#" data-activates="slide-out-right" class="side-nav-left"><i class="fa fa-user"></i></a>
-    </div>
-</div>
-<!-- end navbar top -->
-
-<!-- side nav right-->
-<div class="side-nav-panel-right">
-    <ul id="slide-out-right" class="side-nav side-nav-panel collapsible">
-        <li class="profil">
-            <img src="/img/profile.jpg" alt="">
-            <h2>John Doe</h2>
-        </li>
-        <li><a href="setting.html"><i class="fa fa-cog"></i>Settings</a></li>
-        <li><a href="about-us.html"><i class="fa fa-user"></i>About Us</a></li>
-        <li><a href="contact.html"><i class="fa fa-envelope-o"></i>Contact Us</a></li>
-        <li><a href="login.html"><i class="fa fa-sign-in"></i>Login</a></li>
-        <li><a href="register.html"><i class="fa fa-user-plus"></i>Register</a></li>
-    </ul>
-</div>
-<!-- end side nav right-->
-
-<!-- navbar bottom -->
-<div class="navbar-bottom">
-    <div class="row">
-        <div class="col s2">
-            <a href="index.html"><i class="fa fa-home"></i></a>
-        </div>
-        <div class="col s2">
-            <a href="wishlist.html"><i class="fa fa-heart"></i></a>
-        </div>
-        <div class="col s4">
-            <div class="bar-center">
-                <a href="#animatedModal" id="cart-menu"><i class="fa fa-shopping-basket"></i></a>
-                <span>2</span>
-            </div>
-        </div>
-        <div class="col s2">
-            <a href="contact.html"><i class="fa fa-envelope-o"></i></a>
-        </div>
-        <div class="col s2">
-            <a href="#animatedModal2" id="nav-menu"><i class="fa fa-bars"></i></a>
-        </div>
-    </div>
-</div>
-<!-- end navbar bottom -->
-
+@section('body')
 <!-- menu -->
 <div class="menus" id="animatedModal2">
     <div class="close-animatedModal2 close-icon">
@@ -390,38 +319,7 @@
 <!-- loader -->
 <div id="fakeLoader"></div>
 <!-- end loader -->
-
-<!-- footer -->
-<div class="footer">
-    <div class="container">
-        <div class="about-us-foot">
-            <h6>Mstore</h6>
-            <p>is a lorem ipsum dolor sit amet, consectetur adipisicing elit consectetur adipisicing elit.</p>
-        </div>
-        <div class="social-media">
-            <a href=""><i class="fa fa-facebook"></i></a>
-            <a href=""><i class="fa fa-twitter"></i></a>
-            <a href=""><i class="fa fa-google"></i></a>
-            <a href=""><i class="fa fa-linkedin"></i></a>
-            <a href=""><i class="fa fa-instagram"></i></a>
-        </div>
-        <div class="copyright">
-            <span>© 2017 All Right Reserved</span>
-        </div>
-    </div>
-</div>
-<!-- end footer -->
-
-<!-- scripts -->
-<script src="/js/jquery.min.js"></script>
-<script src="/js/materialize.min.js"></script>
-<script src="/js/owl.carousel.min.js"></script>
-<script src="/js/fakeLoader.min.js"></script>
-<script src="/js/animatedModal.min.js"></script>
-<script src="/js/main.js"></script>
-
-</body>
-</html>
+<script src="/js/jquery-3.3.1.min.js"></script>
 <script>
     $(function (){
         $('#btn').click(function (){
@@ -449,3 +347,9 @@
         })
     })
 </script>
+@endsection
+
+
+@section('bottom')
+    @parent
+@endsection
